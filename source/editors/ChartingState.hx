aeb26032b2b39d5ed432b4c01eb5fae59452a2bd
juanxd20221 <101065582+juanxd20221@users.noreply.github.com>
2022-03-06 20:02:17 -0300

    Add files via upload

--- source/editors/ChartingState.hx
+++ source/editors/ChartingState.hx
@@ -78,7 +78,8 @@ class ChartingState extends MusicBeatState
 		['Alt Idle Animation', "Sets a speciied suffix after the idle animation name.\nYou can use this to trigger 'idle-alt' if you set\nValue 2 to -alt\n\nValue 1: Character to set (Dad, BF or GF)\nValue 2: New suffix (Leave it blank to disable)"],
 		['Screen Shake', "Value 1: Camera shake\nValue 2: HUD shake\n\nEvery value works as the following example: \"1, 0.05\".\nThe first number (1) is the duration.\nThe second number (0.05) is the intensity."],
 		['Change Character', "Value 1: Character to change (Dad, BF, GF)\nValue 2: New character's name"]
-	];
+	    ['Gradient', "Value 1:Play it 1 beat per min/nValue2: Play it 2 beat per min"]
+        ['Lighting Timer', "Value 1:Play it 1 beat per min/nValue2: Play it 2 beat per min"]
 
 	var _file:FileReference;
 
